19becef83d19eefdc6f8ede45fa2dad72757e0fa
jubie <ajuberiya@gmail.com>
2022-02-18 02:15:24 +0530

    updated main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,4 @@
-// SHOW MENU
 
-const showMenu = (toggleId, navId) => {
-      const toggle = document.getElementById(toggleId),
-            nav = document.getElementById(navId)
-
-      if(toggle && nav){
-            toggle.addEventListener('click', () =>{
-                  nav.classList.toggle('show')
-            });
-      }
-}
-
-showMenu('nav_toggle','nav_menu')
 
 // ACTIVE & REMOVE ACTIVE
 const navLink = document.querySelectorAll('.nav_link')
