37da5015b2383fe9ed394d08f9b5dd38dfaeb36a
juberiya <72386146+juberiya1ajaz@users.noreply.github.com>
2022-02-18 02:21:19 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,17 @@
-// ACTIVE & REMOVE ACTIVE
+// SHOW MENU ON MOBILE
+
+const showMenu = (toggleId, navId) => {
+      const toggle = document.getElementById(toggleId),
+            nav = document.getElementById(navId)
+
+      if(toggle && nav){
+            toggle.addEventListener('click', () =>{
+                  nav.classList.toggle('show')
+            });
+      }
+}
+
+showMenu('nav_toggle','nav_menu')// ACTIVE & REMOVE ACTIVE
 const navLink = document.querySelectorAll('.nav_link')
 navLink.forEach(n => n.classList.remove('active'))
 
