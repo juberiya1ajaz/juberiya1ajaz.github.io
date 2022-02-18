6f45e9e0544ad11563ebb6421954edf98175642f
jubie <ajuberiya@gmail.com>
2022-02-18 16:07:43 +0530

    updated main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,17 @@
+// SHOW MENU 
+
+const showMenu = (toggleId, navId) => {
+      const toggle = document.getElementById(toggleId),
+            nav = document.getElementById(navId)
+
+      if(toggle && nav){
+            toggle.addEventListener('click', () =>{
+                  nav.classList.toggle('show')
+            });
+      }
+}
+
+showMenu('nav_toggle','nav_menu')
 
 // ACTIVE & REMOVE ACTIVE
 const navLink = document.querySelectorAll('.nav_link')
